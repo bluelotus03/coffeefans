82e7f1ca1db163ce4d347869f219f21bf29dc6ec
Ash Harmon <25125692+lil-p3tri3@users.noreply.github.com>
2021-06-29 01:57:30 +0000

    added titles, descriptions, removed old pics

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+/* Here's an example script! You can include anything you'd like to include on your website in here! 
+Uncomment the script to have it run! (remove // to uncomment) */
+
+
+//alert("MLH is great for beginners! :D")
